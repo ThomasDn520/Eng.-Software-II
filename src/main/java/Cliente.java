9d018cdf55b2fa19bc766cffe682b52146a074ea
Joao Víctor Batista Barbosa <135074135+KIIRITO5@users.noreply.github.com>
2025-03-16 20:56:30 -0300

    Update Cliente.java

--- src/main/java/Cliente.java
+++ src/main/java/Cliente.java
@@ -19,6 +19,9 @@ public class Cliente {
     public String getSenha() { return senha; }
     public String getCpf() { return cpf; }
 
+    public void setId(int id) { this.id = id; }
     public void setNome(String nome) { this.nome = nome; }
+    public void setEmail(String email) { this.email = email; }
     public void setSenha(String senha) { this.senha = senha; }
+    public void setCpf(String cpf) { this.cpf = cpf; }
 }
