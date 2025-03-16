1c454fc54f937ac2ea4952222614689b415ba713
Joao Víctor Batista Barbosa <135074135+KIIRITO5@users.noreply.github.com>
2025-03-16 20:19:24 -0300

    Update Database.java

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -1,6 +1,7 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Database {
     private static final String URL = "jdbc:sqlite:database.db";
