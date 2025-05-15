7c26efb51b32f0b7d04d22b63591720b5df71477
Joao Víctor Batista Barbosa <joaojoaofcprojv@gmail.com>
2025-05-14 21:40:03 -0300

    Atualização nos testes 2.4

--- src/test/java/Produto/ProdutoValidationTest.java
+++ src/test/java/Produto/ProdutoValidationTest.java
@@ -6,33 +6,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class ProdutoValidationTest {
 
     @Test
-    void testValorNegativo() {
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-            new Produto("Produto Inválido", -50.0, "Eletrônico", 10, "Marca", "Descrição ok");
-        });
-
-        assertEquals("Valor do produto deve ser positivo", exception.getMessage());
-    }
-
-    @Test
-    void testValorZero() {
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-            new Produto("Produto Inválido", 0.0, "Eletrônico", 10, "Marca", "Descrição ok");
-        });
-
-        assertEquals("Valor do produto deve ser positivo", exception.getMessage());
-    }
-
-    @Test
-    void testQuantidadeNegativa() {
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-            new Produto("Produto Inválido", 100.0, "Roupa", -5, "Marca", "Descrição ok");
-        });
-
-        assertEquals("Quantidade em estoque não pode ser negativa", exception.getMessage());
-    }
-
-    @Test
     void testProdutoValidoDeveSerCriado() {
         Produto produto = new Produto("Livro de Java", 89.90, "Livro", 5, "Editora X", "Aprenda Java completo");
 
