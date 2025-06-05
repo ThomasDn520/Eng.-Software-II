2366f7da24305e77721eeaa50edebc259e6275e3
otavio-f <38397566+otavio-f@users.noreply.github.com>
2025-06-05 17:35:43 -0300

    Unifica arquivo JSON Implementa armazenamento de dados em um único arquivo JSON

--- src/main/java/Database/DatabaseJSON.java
+++ src/main/java/Database/DatabaseJSON.java
@@ -1,100 +1,159 @@
 package Database;
 
-import User.*;
-
-import java.io.*;
 import com.google.gson.*;
-import com.google.gson.reflect.TypeToken;
-import java.lang.reflect.Type;
-import java.util.List;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+/**
+ * Classe de database
+ * Armazenamento em JSON no arquivo "database.json" por padrão
+ */
 public class DatabaseJSON {
-
-    public static final String FILE_NAME_ADMIN = "database.json";
-    public static final String FILE_NAME_CLIENTE = "database_cliente.json";
-    public static final String FILE_NAME_LOJA = "database_loja.json";
-    public static final String FILE_NAME_PRODUTOS = "database_produtos.json"; // Opcional
+    private static final Logger logger = Logger.getLogger("JSON");
+    private static String FILE_NAME = "database.json";
 
     private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
-    // Tipos para desserialização (caso necessário)
-    private static final Type ADMIN_LIST_TYPE = new TypeToken<List<UserAdmin>>(){}.getType();
-    private static final Type CLIENTE_LIST_TYPE = new TypeToken<List<UserCliente>>(){}.getType();
-    private static final Type LOJA_LIST_TYPE = new TypeToken<List<UserLoja>>(){}.getType();
-
-    // Inicializar arquivos JSON
+    /**
+     * Inicializa arquivo JSON, populando com as entradas necessárias
+     */
     public static void inicializarJSON() {
-        criarArquivoSeNaoExistir(FILE_NAME_ADMIN, "admins");
-        criarArquivoSeNaoExistir(FILE_NAME_CLIENTE, "clientes");
-        criarArquivoSeNaoExistir(FILE_NAME_LOJA, "lojas");
-        criarArquivoSeNaoExistir(FILE_NAME_PRODUTOS, "produtos");
+        criarArquivoSeNaoExistir("admins");
+        criarArquivoSeNaoExistir("clientes");
+        criarArquivoSeNaoExistir("lojas");
+        criarArquivoSeNaoExistir("produtos");
     }
 
-    private static void criarArquivoSeNaoExistir(String fileName, String arrayName) {
-        File file = new File(fileName);
-        if (!file.exists()) {
-            try (FileWriter writer = new FileWriter(fileName)) {
-                JsonObject root = new JsonObject();
-                root.add(arrayName, new JsonArray());
-                gson.toJson(root, writer);
-                System.out.println("Arquivo " + fileName + " criado com sucesso!");
-            } catch (IOException e) {
-                System.out.println("Erro ao criar " + fileName + ": " + e.getMessage());
-            }
-        }
+    /**
+     * Altera o arquivo padrão para salvamento
+     * @param file O novo arquivo json
+     */
+    public static void setFile(String file) {
+        FILE_NAME = file;
+        inicializarJSON();
+    }
+
+    /**
+     * Cria arquivo JSON e a chave se necessário
+     * @param arrayName Nome da chave do JSON
+     */
+    private static void criarArquivoSeNaoExistir(String arrayName) {
+        JsonArray arr = carregarArray(arrayName);
+
+        if(arr == null) // se a chave não existe, array é nulo
+            arr = new JsonArray(); // salva array vazio
+
+        salvarArray(arrayName, arr);
     }
 
     // Métodos para carregar dados
+
+    /**
+     * Carrega os admins a partir do banco de dados
+     * @return Um array com objetos JSON
+     */
     public static JsonArray carregarAdmins() {
-        return carregarArray(FILE_NAME_ADMIN, "admins");
+        return carregarArray("admins");
     }
 
+    /**
+     * Carrega os clientes a partir do banco de dados
+     * @return Um array com objetos JSON
+     */
     public static JsonArray carregarClientes() {
-        return carregarArray(FILE_NAME_CLIENTE, "clientes");
+        return carregarArray("clientes");
     }
 
+    /**
+     * Carrega as lojas a partir do banco de dados
+     * @return Um array com objetos JSON
+     */
     public static JsonArray carregarLojas() {
-        return carregarArray(FILE_NAME_LOJA, "lojas");
+        return carregarArray("lojas");
     }
 
+    /**
+     * Carrega os produtos a partir do banco de dados
+     * @return Um array com objetos JSON
+     */
     public static JsonArray carregarProdutos() {
-        return carregarArray(FILE_NAME_PRODUTOS, "produtos");
+        return carregarArray("produtos");
     }
 
-    private static JsonArray carregarArray(String fileName, String arrayName) {
-        try (FileReader reader = new FileReader(fileName)) {
+
+    /**
+     * Carrega as entradas de uma chave contida no banco de dados
+     * @param arrayName O nome da chave JSON
+     * @return Um array com objetos JSON
+     */
+    private static JsonArray carregarArray(String arrayName) {
+        try (FileReader reader = new FileReader(FILE_NAME)) {
             JsonObject root = gson.fromJson(reader, JsonObject.class);
             return root.getAsJsonArray(arrayName);
         } catch (IOException e) {
-            System.out.println("Erro ao carregar " + fileName + ": " + e.getMessage());
+            logger.warning("Erro ao carregar " + FILE_NAME + ": " + e.getMessage());
             return new JsonArray();
         }
     }
 
     // Métodos para salvar dados
+    /**
+     * Salva admins no banco de dados
+     * @param admins Um array com objetos JSON
+     */
     public static void salvarAdmins(JsonArray admins) {
-        salvarArray(FILE_NAME_ADMIN, "admins", admins);
+        salvarArray("admins", admins);
     }
 
+    /**
+     * Salva clintes no banco de dados
+     * @param clientes Um array com objetos JSON
+     */
     public static void salvarClientes(JsonArray clientes) {
-        salvarArray(FILE_NAME_CLIENTE, "clientes", clientes);
+        salvarArray("clientes", clientes);
     }
 
+    /**
+     * Salva lojas no banco de dados
+     * @param lojas Um array com objetos JSON
+     */
     public static void salvarLojas(JsonArray lojas) {
-        salvarArray(FILE_NAME_LOJA, "lojas", lojas);
+        salvarArray("lojas", lojas);
     }
 
+    /**
+     * Salva produtos no banco de dados
+     * @param produtos Um array com objetos JSON
+     */
     public static void salvarProdutos(JsonArray produtos) {
-        salvarArray(FILE_NAME_PRODUTOS, "produtos", produtos);
+        salvarArray("produtos", produtos);
     }
 
-    private static void salvarArray(String fileName, String arrayName, JsonArray array) {
-        try (FileWriter writer = new FileWriter(fileName)) {
-            JsonObject root = new JsonObject();
+    /**
+     * Salva um array de objetos no banco de dados
+     * @param arrayName A chave do objeto JSON
+     * @param array O array a ser salvo
+     */
+    private static void salvarArray(String arrayName, JsonArray array) {
+        // ler json existente
+        JsonObject root;
+        try (FileReader reader = new FileReader(FILE_NAME)) {
+            root = gson.fromJson(reader, JsonObject.class);
+        } catch (IOException e) {
+            // não conseguiu ler o arquivo, talvez não exista ou não tenha permissão suficiente
+            logger.warning("Erro ao ler " + FILE_NAME + ": " + e.getMessage());
+            root = new JsonObject();
+        }
+
+        // adiciona novo array no json existente e salva
+        try (FileWriter writer = new FileWriter(FILE_NAME)) {
             root.add(arrayName, array);
             gson.toJson(root, writer);
         } catch (IOException e) {
-            System.out.println("Erro ao salvar " + fileName + ": " + e.getMessage());
+            logger.warning("Erro ao salvar " + FILE_NAME + ": " + e.getMessage());
         }
     }
 }
